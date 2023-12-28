782e69570859546667e8c94d462f702f658b8a6d
neftinformatica <98559137+neftinformatica@users.noreply.github.com>
2023-12-28 18:27:49 +0000

    Add files via upload

---
+++ precache-manifest.be13c21a7961ab7a534e94efabfdbf89.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcd1a17a78cdff8c9a8b",
+    "url": "/static/css/main.777447e4.chunk.css"
+  },
+  {
+    "revision": "dcd1a17a78cdff8c9a8b",
+    "url": "/static/js/main.62f6b109.chunk.js"
+  },
+  {
+    "revision": "d7de795598575922bf8f",
+    "url": "/static/js/runtime~main.585d1ad4.js"
+  },
+  {
+    "revision": "0d4cf7fb60555ebf9e54",
+    "url": "/static/js/2.4ad6dbd1.chunk.js"
+  },
+  {
+    "revision": "6aeca4f8cdaa3239bdcb",
+    "url": "/static/js/3.34522cac.chunk.js"
+  },
+  {
+    "revision": "19d7d14732f30fa9f4da496e730be4a9",
+    "url": "/static/media/fixing.19d7d147.svg"
+  },
+  {
+    "revision": "7795a1eee22ab8f0340809c80603f76d",
+    "url": "/static/media/time.7795a1ee.svg"
+  },
+  {
+    "revision": "2e9d3508d1e47e417173edbbed67988d",
+    "url": "/static/media/maintenance.2e9d3508.svg"
+  },
+  {
+    "revision": "ee619215e9035a575108c0c5c134e596",
+    "url": "/static/media/server.ee619215.svg"
+  },
+  {
+    "revision": "a4f1c15239c194e0d41f7b2eef291ab2",
+    "url": "/static/media/remotely.a4f1c152.svg"
+  },
+  {
+    "revision": "8fbb3d30d71e0a0e50f914a27ea9aa66",
+    "url": "/static/media/logo.8fbb3d30.svg"
+  },
+  {
+    "revision": "bb653cb17f410c4d65af9430c9eb7e35",
+    "url": "/index.html"
+  }
+];
